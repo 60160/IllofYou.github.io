48261e7ca71d8173a94c1b4596a16f15c2af9ac2
60160 <illofyou111416@gmail.com>
2025-10-31 19:09:12 -0700

    holy shit im a legend

--- pageflipper.js
+++ pageflipper.js
@@ -1,5 +1,18 @@
 	let pagecount = 1;
+	let nchapter=chapternumber-1;
+	let pchapter=chapternumber+1;
+	if(pchapter >12){pchapter=12;}
+	document.getElementById("pchap").href="Chapter"+pchapter+".html";
+	document.getElementById("nchap").href="Chapter"+nchapter+".html";
+	pageselecter=document.getElementById("pageselect");
+	for (var i=1; i<=pagemax; i++){
+		var opt = document.createElement('option');
+		opt.value = i;
+		opt.innerHTML = i;
+		pageselecter.appendChild(opt);
+	}
 	document.addEventListener('keydown', function(e) {
+		e.preventDefault();
 		if(e.code == "ArrowRight") {
 			flipforward();
 		}
@@ -14,6 +27,7 @@
 			chapternumber--;
 			window.location.replace("Chapter"+chapternumber+".html");
 		}
+		pageselecter.selectedIndex++;
 		let pageindex = ("000"+pagecount).slice(-3);
 		document.getElementById("chapterpage").src=imgURLbase+pageindex+".png";
 	};
@@ -26,7 +40,11 @@
 			}
 			window.location.replace("Chapter"+chapternumber+".html");
 		}
+		pageselecter.selectedIndex--;
 		let pageindex = ("000"+pagecount).slice(-3);
 		document.getElementById("chapterpage").src=imgURLbase+pageindex+".png";
 	};
-	
+	function setpage() {
+	pagecount=pageselecter.value;
+	document.getElementById("chapterpage").src=imgURLbase+("000"+pageselecter.value).slice(-3)+".png";
+	}
