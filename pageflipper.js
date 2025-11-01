c3407c8075270a57c993ebf6109ba961307651d8
60160 <illofyou111416@gmail.com>
2025-10-31 17:03:40 -0700

    better js + nav buttons work now

--- pageflipper.js
+++ pageflipper.js
@@ -1,45 +1,32 @@
 	let pagecount = 1;
-	document.getElementById("chapterpage").addEventListener("click", function() {
-		pagecount++;
-		if(pagecount > pagemax){
-			chapternumber++;
-			window.location.replace("Chapter"+chapternumber+".html");
-		}
-		else if(pagecount < pagemax){
-			chapternumber--;
-			window.location.replace("Chapter"+chapternumber+".html");
-		}
-		let pageindex = ("000" + pagecount).slice(-3);
-		this.src=imgURLbase+pageindex+".png";
-	});
-/*	
-	document.onkeydown = function (e) {
-		e = e || window.event;
-		pagecount++;
-		let pageindex = ("000" + pagecount).slice(-3);
-		document.getElementById("chapterpage").src=imgURLbase+pageindex+".png";
-		};
-*/	
-	
 	document.addEventListener('keydown', function(e) {
 		if(e.code == "ArrowRight") {
-			pagecount++;
+			flipforward();
 		}
 		else if(e.code == "ArrowLeft")  {
-			pagecount--;
+			flipbackward();
 		}
+	});
+	
+	function flipforward() {
+		pagecount++;
 		if(pagecount > pagemax){
 			chapternumber--;
 			window.location.replace("Chapter"+chapternumber+".html");
 		}
-		else if(pagecount < 1){
+		let pageindex = ("000"+pagecount).slice(-3);
+		document.getElementById("chapterpage").src=imgURLbase+pageindex+".png";
+	};
+	function flipbackward() {
+		pagecount--;
+		if(pagecount < 1){
 			chapternumber++;
 			if(chapternumber>12){
 				chapternumber--;
 			}
 			window.location.replace("Chapter"+chapternumber+".html");
 		}
-		let pageindex = ("000" + pagecount).slice(-3);
+		let pageindex = ("000"+pagecount).slice(-3);
 		document.getElementById("chapterpage").src=imgURLbase+pageindex+".png";
-	});
+	};
 	
